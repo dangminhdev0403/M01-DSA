edd593c233b5cf010ced1ed4dd3e07f530dbe730
Your Name <minhphp2005@gmail.com>
2024-09-26 23:17:47 +0700

    biến thể binarySearch

--- src/App.java
+++ src/App.java
@@ -1,37 +1,69 @@
 import java.util.*;
 
-/**
- * App
- */
 public class App {
+
+    public static int firstPos(List<Integer> arr, int x) {
+        int l = 0;
+        int r = arr.size() - 1;
+
+        int index = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr.get(mid) == x) {
+                index = mid;
+                l = mid + 1;
+            } else if (arr.get(mid) < x) {
+                l = mid + 1;
+
+            } else {
+                r = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    public static int lastPos(List<Integer> arr, int x) {
+        int l = 0;
+        int r = arr.size() - 1;
+
+        int index = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr.get(mid) == x) {
+                index = mid;
+                l = mid + 1;
+            } else if (arr.get(mid) > x) {
+                r = mid - 1;
+
+            } else {
+                l = mid + 1;
+            }
+        }
+        return index;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n  =sc.nextInt() ;
-        
-        List <Integer> arr = new ArrayList<>() ;
-        for(int i = 0 ; i < n ; i++){
+        List<Integer> arr = new ArrayList<>();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
             arr.add(sc.nextInt());
         }
 
-        //  Collections.sort(arr) ;   sắp xếp  Tăng dần 
+        Collections.sort(arr);
 
-      Collections.sort(arr,new Comparator<Integer>() {
-
-        @Override
-        public int compare(Integer a, Integer b) {
-            //  -1  a đứng trước b
-            //  1  a đứng sau b
-            //  0 giữ im vị trí
-            if(a > b) return -1;
-
-            else return 1 ;
+        int tongThieu = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            int thieu = k - arr.get(i);
+            int fisrtThieu = firstPos(arr, thieu);
+            if(fisrtThieu != -1){
+                int lastThieu = lastPos(arr, thieu);
+                tongThieu += lastThieu - fisrtThieu + 1 ;
+            }
         }
 
-        
-      }) ;
-        for(int x : arr){
-            System.out.print(x+" ");
-        } 
+        System.out.println(tongThieu);
     }
 }
