d44511783db55ce23c758fb30453bd260b0ac6e3
Your Name <minhphp2005@gmail.com>
2024-09-27 22:38:55 +0700

    Set

--- src/App.java
+++ src/App.java
@@ -1,69 +1,27 @@
 import java.util.*;
+// 3 cái Set  chỉ lưu các phần tử khác nhau
+// HashSet không sắp xếp , thứ tự phần tử xuất hiện ngẫu nhiễn
+// LinkedHashSet cái nào nhập trước in ra trước
+// TreeSet sắp xếp thứ tự tăng dần
+
 
 public class App {
-
-    public static int firstPos(List<Integer> arr, int x) {
-        int l = 0;
-        int r = arr.size() - 1;
-
-        int index = -1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (arr.get(mid) == x) {
-                index = mid;
-                l = mid + 1;
-            } else if (arr.get(mid) < x) {
-                l = mid + 1;
-
-            } else {
-                r = mid - 1;
-            }
-        }
-        return index;
-    }
-
-    public static int lastPos(List<Integer> arr, int x) {
-        int l = 0;
-        int r = arr.size() - 1;
-
-        int index = -1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (arr.get(mid) == x) {
-                index = mid;
-                l = mid + 1;
-            } else if (arr.get(mid) > x) {
-                r = mid - 1;
-
-            } else {
-                l = mid + 1;
-            }
-        }
-        return index;
-    }
-
     public static void main(String[] args) {
+        int a;
         Scanner sc = new Scanner(System.in);
-        List<Integer> arr = new ArrayList<>();
+        // HashSet<Integer> set = new HashSet<>();
+        // LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
+
         int n = sc.nextInt();
-        int k = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
-            arr.add(sc.nextInt());
+            a = sc.nextInt();
+            set.add(a);
         }
 
-        Collections.sort(arr);
-
-        int tongThieu = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            int thieu = k - arr.get(i);
-            int fisrtThieu = firstPos(arr, thieu);
-            if(fisrtThieu != -1){
-                int lastThieu = lastPos(arr, thieu);
-                tongThieu += lastThieu - fisrtThieu + 1 ;
-            }
+        for(int x : set){
+            System.out.print(x+" ");
         }
 
-        System.out.println(tongThieu);
     }
 }
